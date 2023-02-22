de7dae6563f321054aa9ea8981eeeb6f937d3f14
Igor_Popovich <78104120+IgorPopovich@users.noreply.github.com>
2023-02-22 10:13:48 +0200

    commit

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,17 @@
+import './FriendList.css'
+
+const FriendList = ({ friends }) => {
+  return (
+    <ul className="friend-list">
+        {friends.map((item, i) => {
+            return <li key={item.id} className="item">
+                        <span className="status" style={{backgroundColor: item.isOnline ? 'red':'green'}}></span>
+                        <img className="avatar" src={item.avatar} alt="User avatar" width="48" />
+                        <p className="name">{item.name}</p>
+                    </li>
+        })}
+    </ul>
+  );
+};
+
+export default FriendList;
